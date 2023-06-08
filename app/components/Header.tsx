4f3ed5a1a9851759dcd418beecdf7ec5501944cc
umermemon4648 <umermemon4648@gmail.com>
2023-06-08 10:27:40 +0500

    write styling description from figma

---
+++ app/components/Header.tsx
@@ -0,0 +1,53 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <>
+      <nav className='flex flex-row items-center'>
+
+<div className="logo">
+
+{/* N */}
+<svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M14.9441 0.956055V13.0222L4.21856 0.956055H0.799805V17.0443H3.74932V5.17922L14.4748 17.0443H17.8936V0.956055H14.9441Z" fill="#00BCD4"/>
+</svg>
+
+{/* E */}
+<svg width="17" height="18" viewBox="0 0 17 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M0.562988 0.956055H16.3831V3.7715H3.5125V7.65949H14.7072V10.4079H3.5125V14.2959H16.3831V17.0443H0.562988V0.956055Z" fill="#00BCD4"/>
+</svg>
+
+
+{/* W */}
+<svg width="27" height="18" viewBox="0 0 27 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M0.360352 0.956055L6.66158 17.0443H8.94075L13.4991 5.51439L18.0574 17.0443H20.3366L26.6378 0.956055H23.4202L19.264 11.6816L14.9738 0.956055H12.0243L7.80117 11.6816L3.51097 0.956055H0.360352Z" fill="#00BCD4"/>
+</svg>
+
+
+{/* X */}
+<svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M0.0786133 0.956055V1.69343L6.58095 9.00018L0.0786133 16.3069V17.0443H3.49737L8.59198 11.2793L13.7536 17.0443H17.1724V16.3069L10.603 9.00018L17.1724 1.69343V0.956055H13.7536L8.59198 6.65398L3.49737 0.956055H0.0786133Z" fill="#00BCD4"/>
+</svg>
+
+
+
+
+{/* L */}
+<svg width="17" height="18" viewBox="0 0 17 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M0.979492 0.956055V17.0443H16.7996V14.2959H3.929V0.956055H0.979492Z" fill="#00BCD4"/>
+</svg>
+
+{/* inverse E */}
+<svg width="16" height="18" viewBox="0 0 16 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M15.908 0.956055H0.0878906V3.7715H12.9585V7.65949H1.76375V10.4079H12.9585V14.2959H0.0878906V17.0443H15.908V0.956055Z" fill="#00BCD4"/>
+</svg>
+</div>
+
+
+
+      </nav>
+    </>
+  )
+}
+
+export default Header
